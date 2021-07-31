0ccedfa97db73420f135394e1ec87266a5be3514
andrew9488 <pashkevichandrew@gmail.com>
2021-07-31 11:01:31 +0300

    feat: create simple UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [messages, setMessages] = useState([
+        {user: {id: 1, name: "Andrew"}, message: "hello Anton", id: 1.1},
+        {user: {id: 2, name: "Anton"}, message: "Hi, Andrew!", id: 2.1}
+    ])
+
+    return (
+        <div className="App">
+            <div style={{
+                border: "1px solid",
+                padding: 10,
+                height: 300,
+                width: 250,
+                overflowY: "scroll"
+            }}>
+                {messages.map(m => {
+                    return <div key={m.user.name + m.id}>
+                        <b>{m.user.name}: </b> <span>{m.message}</span>
+                        <hr/>
+                    </div>
+
+                })}
+            </div>
+            <textarea/>
+            <button>send</button>
+        </div>
+    );
 }
 
 export default App;
