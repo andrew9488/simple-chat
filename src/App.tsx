43bdd9b8ccf8a207f9b39ada3081810f79f38017
andrew9488 <pashkevichandrew@gmail.com>
2021-07-31 17:42:09 +0300

    feat: connect to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
+import {io} from "socket.io-client";
+
+// let socket = io("https://simple-chat-back.herokuapp.com", {transports: ['websocket', 'polling', 'flashsocket']})
+let socket = io("http://localhost:3009", {transports: ['websocket', 'polling', 'flashsocket']})
 
 function App() {
 
+    useEffect(() => {
+    }, [])
+
+    const [message, setMessage] = useState("")
     const [messages, setMessages] = useState([
         {user: {id: 1, name: "Andrew"}, message: "hello Anton", id: 1.1},
         {user: {id: 2, name: "Anton"}, message: "Hi, Andrew!", id: 2.1}
     ])
+
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+
+    const onClickHandler = () => {
+        if (message.trim() !== "") {
+            socket.emit("message-sent", message)
+            setMessage("")
+        }
+    }
 
     return (
         <div className="App">
@@ -25,8 +44,8 @@ function App() {
 
                 })}
             </div>
-            <textarea/>
-            <button>send</button>
+            <textarea value={message} onChange={onChangeHandler}/>
+            <button onClick={onClickHandler}>send</button>
         </div>
     );
 }
