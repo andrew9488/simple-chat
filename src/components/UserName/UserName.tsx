329ded9f39a99b0ce6a04791ad8dbe0d2fb8e56c
andrew9488 <pashkevichandrew@gmail.com>
2021-08-03 21:04:03 +0300

    fix: add routes

--- src/components/UserName/UserName.tsx
+++ src/components/UserName/UserName.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import {useStyles} from "./materialUIstyles";
 import {Button, Card, CardActions, CardContent} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
+import {useHistory} from "react-router-dom";
+import {PATH} from "../Routes/Routes";
 
 export const UserName: React.FC = React.memo(() => {
     const classes = useStyles()
+    const history = useHistory()
+
+    const onClickWantEnterName = () => {
+        history.push(PATH.ENTER_USER_NAME)
+    }
+    const onClickCancelEnterName = () => {
+        history.push(PATH.CHAT)
+    }
+
     return (
         <Card className={classes.root}>
             <CardContent>
@@ -13,8 +24,8 @@ export const UserName: React.FC = React.memo(() => {
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button variant="contained" color="primary" size="medium">Yes</Button>
-                <Button variant="contained" color="secondary" size="medium">No</Button>
+                <Button variant="contained" color="primary" size="medium" onClick={onClickWantEnterName}>Yes</Button>
+                <Button variant="contained" color="secondary" size="medium" onClick={onClickCancelEnterName}>No</Button>
             </CardActions>
         </Card>
     );
