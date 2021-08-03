05a695ef0be1dcecbf556acf145d138f4f9176cf
andrew9488 <pashkevichandrew@gmail.com>
2021-08-03 20:33:05 +0300

    feat: create Routes

---
+++ src/components/Routes/Routes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import {UserName} from "../common/UserName/UserName";
+import {EnterUserName} from "../EnterUserName";
+import {Chat} from "../../features/Chat/Chat";
+
+const PATH = {
+    USER_NAME: "/",
+    ENTER_USER_NAME: "/enter_user_name",
+    CHAT: "/chat"
+}
+
+export const Routes: React.FC = React.memo(() => {
+    return (
+        <Switch>
+            <Route exact path={PATH.USER_NAME} render={() => <UserName/>}/>
+            <Route path={PATH.ENTER_USER_NAME} render={() => <EnterUserName/>}/>
+            <Route path={PATH.CHAT} render={() => <Chat/>}/>
+        </Switch>
+    );
+})
